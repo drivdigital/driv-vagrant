c87471cf57fa18084774078f8a03385b05268ccb
Eivin Landa <landa@drivdigital.no>
2015-09-17 09:58:38 +0200

    Silenced wp-cli download

--- config/tmp-fix-wpcli.php
+++ config/tmp-fix-wpcli.php
@@ -3,6 +3,6 @@
 // A temporary way to add wpcli
 // (Until the box is rebuilt with a propper apache config file)
 if ( !file_exists( 'wp-cli.phar' ) )
-  `curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar`;
+  `curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -s`;
 if ( file_exists( 'wp-cli.phar' ) && !file_exists( '/usr/local/bin/wp' ) )
   `sudo ln -s /vagrant/wp-cli.phar /usr/local/bin/wp`;
